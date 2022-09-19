76a3d563adb52a3b31c23e654914539aa98454fe
Sabbisetti123 <sahithsabbisetti@gmail.com>
2022-09-19 19:16:29 +0530

    added succesfully

---
+++ src/main/java/checkjenkins/Welcome.java
@@ -0,0 +1,10 @@
+package checkjenkins;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                 System.out.println("Welcoem to jenkins");
+	}
+
+}
